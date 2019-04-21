694f70c46848ca66d0b253baeb638cb6a6ffaf98
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-21 03:47:28 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -44,7 +44,9 @@ class SongsController < ApplicationController
   private 
   
   def set_song
-    
+    @song = Song.find(params[:id])
+  end 
+  
   def song_params 
     params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
   end 
